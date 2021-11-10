83111ee585f038f4b4857a32d85484759649c370
zowsky24 <zowsky24@naver.com>
2021-11-10 09:46:12 +0900

    Revert "Revert "수정""
    This reverts commit e3cc153f84fd5d73e09603b59a941c68d38ee330.

--- api/index.js
+++ api/index.js
@@ -1,20 +1,32 @@
 import axios from 'axios';
 
-const instance = axios.create({
-	baseURL: 'http://localhost:3000'
+const products = axios.create({
+	baseURL: 'http://localhost:3000/products/'
+})
+const carts = axios.create({
+	baseURL: 'http://localhost:3000/carts/'
 })
 
 function fetchProductById(id) {
-	return instance.get(`/products/${id}`) // get은 baseURL을 기본적으로 깔고감. 그 이후 url만 적어주면 됨
+	return products.get(`${id}`) // get은 baseURL을 기본적으로 깔고감. 그 이후 url만 적어주면 됨
 }
 
 function fetchProductsByKeyword(keyword) {
 	// return instance.get(`/products?name_like=${keyword}`)
-	return instance.get(`/products`, {
+	return products.get(``, {
 		params: {
 			name_like: keyword,
 		}
 	})
 }
 
-export { fetchProductById, fetchProductsByKeyword }
+// cart
+function fetchCartItems() {
+	return carts.get('')
+}
+
+function createCartItem(cartItem) {
+	return carts.post(``, cartItem) // 두번째 값은 넘겨줄 값
+}
+
+export { fetchProductById, fetchProductsByKeyword, createCartItem, fetchCartItems }
